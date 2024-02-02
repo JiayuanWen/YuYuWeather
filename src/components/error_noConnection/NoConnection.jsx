6f52d45d6364bcab6e0d42703a792a77496cce9b
JiayuanWen <91281147+jiayuanwen@users.noreply.github.com>
2024-02-02 17:49:38 -0500

    Added pop up window for Settings menu

--- src/components/error_noConnection/NoConnection.jsx
+++ src/components/error_noConnection/NoConnection.jsx
@@ -4,8 +4,8 @@ import wifiOffLogo from './wifi_off.svg';
 
 
 function NoConnectionPrompt({status}) { // Component with parameters. See https://react.dev/learn/conditional-rendering
-    console.log(status)
-    if (status.indexOf("Network Error") !== -1 || status.indexOf("Network error") !== -1) {
+
+    if (String(status).indexOf("Network Error") !== -1 || String(status).indexOf("Network error") !== -1) {
         return (
             <div className="no-connection-prompt">
                 <img className="no-connection-prompt-icon" src={wifiOffLogo} alt="no-wifi"/>
--- src/components/function_settings/Settings.jsx
+++ src/components/function_settings/Settings.jsx
@@ -1,24 +1,56 @@
-import React from "react";
+import React, { useState } from "react";
+
+import settingsIcon from './settings.svg'
+
 import './settings.css';
 
 export default function Settings() {
+    const [settings_visible, setSettingsVisible] = useState(false);
 
+    // Settings icon
+    const settingsIconClick = (event) => {
+        settings_visible ?
+            setSettingsVisible(false)
+        :
+            setSettingsVisible(true)
+
+    }
     const setting_icon = (
-        <button className="settings-icon">
-            <ion-icon name="settings"></ion-icon>
+        <button className="settings-icon" onClick={function(e) {settingsIconClick(e);}}>
+            <img src={settingsIcon} alt="settings-icon"></img>
         </button>
     );
-
+    
+    // Settings menu
+    const settings_style_visible = {
+        opacity: "1",
+        pointerEvents: "all"
+    }
+    const settings_style_hide = {
+        opacity: "0",
+        pointerEvents: "none"
+    }
     const settings = (
-        <div className="settings-menu">
-            
-        </div>
+        settings_visible ?
+            <div style={settings_style_visible} className="settings-overlay">
+                <div className="settings-menu">
+                
+                </div>
+            </div>
+        :
+            <div style={settings_style_hide} className="settings-overlay">
+                <div className="settings-menu">
+                
+                </div>
+            </div>
     );
 
     // Final output //
     return (
         <>
             {setting_icon}
+
+            {settings}
         </>
     );
 
